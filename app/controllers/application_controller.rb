fbdb1dc41dde55ae0f87b0832e8650dbfc9b8c8d
thechanmoon <thechanmoon@gmail.com>
2020-03-11 11:19:43 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,8 +21,10 @@ class ApplicationController < Sinatra::Base
     redirect to "/articles/#{ @article.id }"
   end
 
-  get '/articles/show' do
-  end
+  # get "/articles/:id" do
+  #   @article = Article.find(params[:id])
+  #   erb :show
+  # end
 
   get "/articles" do
     @articles = Article.all
@@ -34,9 +36,18 @@ class ApplicationController < Sinatra::Base
     erb :show
   end
   
-  get '/articles/:id/edit' do
+  get "/articles/:id/edit" do
     @article = Article.find(params[:id])
     erb :edit
+  end
+
+  patch "/articles/:id" do
+    @article = Article.find(params[:id])
+    # @article.update(params[:article])
+    @article.title = params[:title]
+    @article.content = params[:content]
+    @article.save    
+    redirect to "/articles/#{ @article.id }"
   end
 
   delete "/articles/:id" do
